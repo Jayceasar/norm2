8f6d7bcef6957ab9c11d9ff85958a865b73ce644
Jayceasar <juliusiniekong@gmail.com>
2023-10-17 08:56:41 +0100

    useQuery

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,18 @@
 import { Product } from "@prisma/client";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [dataProducts, setDataProducts] = useState<Product[] | null>(null);
   // fetch all products on page load
-  // const { data: dataProducts, isLoading: isLoadingProducts } = useQuery<
-  //   Product[]
-  // >({
-  //   queryKey: ["products"],
-  //   queryFn: async () => {
-  //     const response = await axios.get("/api/products");
-  //     return response.data;
-  //   },
-  // });
+  const { data: dataProducts, isLoading: isLoadingProducts } = useQuery<
+    Product[]
+  >({
+    // queryKey: ["products"],
+    queryFn: async () => {
+      const response = await axios.get("/api/products");
+      return response.data;
+    },
+  });
 
   // load animation
   const { mutate: animationData, isLoading: isLoadingAnimation } = useMutation({
@@ -31,32 +29,27 @@ export default function Home() {
     },
   });
 
-  useEffect(() => {
-    async function fetchProducts() {
-      const response = await axios.get("/api/products");
-      setDataProducts(response.data);
-      console.log(response.data);
-    }
-    fetchProducts();
-  }, []);
-
   return (
     <>
-      <div>
-        {dataProducts?.map((product, i) => {
-          return (
-            <button
-              className=" p-4 border-2 border-black w-fit"
-              key={i}
-              onClick={() => {
-                animationData(product.id);
-              }}
-            >
-              <p>{product.title}</p>
-            </button>
-          );
-        })}
-      </div>
+      {isLoadingProducts ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {dataProducts?.map((product, i) => {
+            return (
+              <button
+                className=" p-4 border-2 border-black w-fit"
+                key={i}
+                onClick={() => {
+                  animationData(product.id);
+                }}
+              >
+                <p>{product.title}</p>
+              </button>
+            );
+          })}
+        </div>
+      )}
 
       {isLoadingAnimation ? <p>Loading...</p> : <div>Found data</div>}
     </>
