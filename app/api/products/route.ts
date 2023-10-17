cb3ca889673d87c1eaa2b7dd8bce6a09edf91469
Jayceasar <juliusiniekong@gmail.com>
2023-10-17 09:10:56 +0100

    no-cache

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -4,7 +4,16 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     const products = await prisma.product.findMany();
-    return NextResponse.json(products, { status: 200 });
+
+    // Set Cache-Control header to no-cache
+    const responseHeaders = {
+      "Cache-Control": "no-cache",
+    };
+
+    return NextResponse.json(products, {
+      status: 200,
+      headers: responseHeaders,
+    });
   } catch (error) {
     return NextResponse.json(error, { status: 500 });
   }
